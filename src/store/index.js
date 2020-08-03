db31d4a678ac15255332d0fcf56221abb1ad657e
HninnHninn <hninnthazinmyint820@gmail.com>
2020-08-03 14:27:06 +0630

    second commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,42 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import prices from './prices';
 
+// configure Vuex for modules
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+const store = new Vuex.Store({
+  state: {
+    prices: prices
+  },
+  getters: {
+    currentPrice: state => {
+      return state.prices[0];
+    },
+    previousPrice: state => {
+      return state.prices[1];
+    },
+    percentageIncrease: (state, getters) => {
+      const currentAmount = getters.currentPrice.amount;
+      const previousAmount = getters.previousPrice.amount;
+      return (
+        ((currentAmount - previousAmount) / previousAmount) *
+        100
+      ).toFixed(2);
+    },
+    difference: (state, getters) => {
+      const currentAmount = getters.currentPrice.amount;
+      const previousAmount = getters.previousPrice.amount;
+      return (currentAmount - previousAmount).toFixed(2);
+    }
+  },
+  mutations: {
+    UPDATE_PRICE(state, newPricing) {
+      state.prices.pop();
+      state.prices = [newPricing, ...state.prices];
+    }
+  }
 });
+
+export default store;
+export {default as actions} from './actions';
